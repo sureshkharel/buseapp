83a5debba133523e155b200f3fac0b9bb34dfe4a
sureshkharel <suresh.sukha@gmail.com>
2020-06-22 20:22:13 +1000

    Add files via upload
    adding build folder to test in ibm cloud

---
+++ build/precache-manifest.5ab3216b9429e4ea7df695a5ea373b67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7703cab08e2db6b0ed8e3ab7da36031",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ec75119d949dd63099a",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2035f6bda3cf1bf1bbda",
+    "url": "/static/js/2.28fa4db1.chunk.js"
+  },
+  {
+    "revision": "4c18248df8da7fd3308ca755ae4fecc0",
+    "url": "/static/js/2.28fa4db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec75119d949dd63099a",
+    "url": "/static/js/main.f4845b73.chunk.js"
+  },
+  {
+    "revision": "ee043602c69f02efe8df",
+    "url": "/static/js/runtime-main.c350213e.js"
+  },
+  {
+    "revision": "1a935e82ace532dc7e6cb0592a1fe867",
+    "url": "/static/media/banner.1a935e82.jpg"
+  },
+  {
+    "revision": "547b7138a62d325698ca9574699a0b60",
+    "url": "/static/media/seeproduct.547b7138.jpg"
+  }
+]);
